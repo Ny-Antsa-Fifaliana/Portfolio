6f1e6b7bc79dc906167c8563140feb4ff18fbb1d
Fifa <nyantsafifaliana240@gmail.com>
2024-03-18 23:09:33 +0300

    add parallax

--- script.js
+++ script.js
@@ -65,7 +65,27 @@
         navlist.classList.remove("open");
     }
 
-    
+    // parallax about js
+    {
+        function revealElements() {
+            var reveals = document.querySelectorAll('.parallax');
+        
+            for (var i = 0; i < reveals.length; i++) {
+                var windowHeight = window.innerHeight;
+                var elementTop = reveals[i].getBoundingClientRect().top;
+                var elementVisible = 150;
+        
+                if (elementTop < windowHeight - elementVisible) {
+                    reveals[i].classList.add('active');
+                } else {
+                    reveals[i].classList.remove('active');
+                }
+            }
+        }
+        
+        window.addEventListener('scroll', revealElements);
+    }
+
 
 
 //  ------------  JQUERY  ------------------------
